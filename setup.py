c3887cb5082f37e3471d40fe9ae5e48e42b39453
Anis Koubaa <aniskoubaa@users.noreply.github.com>
2023-12-19 11:16:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,46 @@
 from setuptools import setup, find_packages
 
+# Long description from README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='aranizer',
-    version='0.1',
+    version='0.1.8',
+    author='RIOTU Lab',
+    author_email='riotu@psu.edu.sa',
+    description='Aranizer: A Custom Tokenizer for Enhanced Arabic Language Processing',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/riotu-lab/aranizer',
     packages=find_packages(),
-    url='https://github.com/omarnj-lab/aranizer',
-    license='MIT',
-    author='omar najar',
-    author_email='onajar@psu.edu.sa',
-    description='Aranizer:A Custom Tokenizer for Enhanced Arabic Language Processing',
     install_requires=[
-        'transformers',
-        'sentence_transformers'
+        'transformers>=4.0.0',
+        'sentence-transformers>=0.4.0'
     ],
-    include_package_data=True
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: Arabic',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    license='Apache 2.0',
+    include_package_data=True,
+    # Optional: if your package offers a CLI
+    # entry_points={
+    #     'console_scripts': [
+    #         'aranizer-cli=aranizer.command_line:main',
+    #     ],
+    # },
 )
+
+
