a6e3eb37ef898a72790ef2e0c59215878dfb9af6
Eng. Omar Najar <79968243+omarnj-lab@users.noreply.github.com>
2024-07-07 13:56:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='aranizer',
-    version='0.1.8',
+    version='0.1.9',
     author='RIOTU Lab',
     author_email='riotu@psu.edu.sa',
     description='Aranizer: A Custom Tokenizer for Enhanced Arabic Language Processing',
