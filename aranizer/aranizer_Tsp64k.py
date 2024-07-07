49e26f0598d5b2ec036ec02f482f761832063745
Eng. Omar Najar <79968243+omarnj-lab@users.noreply.github.com>
2024-07-07 13:51:02 +0300

    Create aranizer_Tsp64k.py

---
+++ aranizer/aranizer_Tsp64k.py
@@ -0,0 +1,19 @@
+from transformers import PreTrainedTokenizerFast
+
+def get_tokenizer():
+    """
+    Initializes and returns a custom tokenizer for Arabic language processing.
+
+    This function loads a tokenizer from a predefined file and enriches its
+    vocabulary with a set of Arabic diacritics as special tokens. The tokenizer
+    is based on the PreTrainedTokenizerFast class from the transformers library.
+
+    Returns:
+        PreTrainedTokenizerFast: A tokenizer with extended vocabulary to
+                                 include Arabic diacritics.
+    """
+    # Initialize the tokenizer
+    tokenizer_fast = PreTrainedTokenizerFast(tokenizer_file="aranizer/TSP_tokenizer_64.0K.json")
+
+
+    return tokenizer_fast
