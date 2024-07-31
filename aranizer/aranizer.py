c03bf8f200dd676aa35d38ba4f23d414b3d3b520
Serry Sibaee <77074626+serrysibaee@users.noreply.github.com>
2024-07-31 13:30:58 +0300

    Update aranizer.py
    updating the names of sentences piece files from 'model' to 'json'

--- aranizer/aranizer.py
+++ aranizer/aranizer.py
@@ -7,18 +7,18 @@ dc = {
     "bpe50": "tokenizers/bpe/BPE_tokenizer_50.0K.json",
     "bpe64": "tokenizers/bpe/BPE_tokenizer_64.0K.json",
     "bpe86": "tokenizers/bpe/BPE_tokenizer_86.0K.json",
-    "sp32": "tokenizers/sp/SP_tokenizer_32.0K.model",
-    "sp50": "tokenizers/sp/SP_tokenizer_50.0K.model",
-    "sp64": "tokenizers/sp/SP_tokenizer_64.0K.model",
-    "sp86": "tokenizers/sp/SP_tokenizer_86.0K.model",
+    "sp32": "tokenizers/sp/SP_tokenizer_32.0K.json",
+    "sp50": "tokenizers/sp/SP_tokenizer_50.0K.json",
+    "sp64": "tokenizers/sp/SP_tokenizer_64.0K.json",
+    "sp86": "tokenizers/sp/SP_tokenizer_86.0K.json",
     "bpe32T": "tokenizers/bpe/TBPE_tokenizer_32.0K.json",
     "bpe50T": "tokenizers/bpe/TBPE_tokenizer_50.0K.json",
     "bpe64T": "tokenizers/bpe/TBPE_tokenizer_64.0K.json",
     "bpe86T": "tokenizers/bpe/TBPE_tokenizer_86.0K.json",
     "sp32T": "tokenizers/sp/TSP_tokenizer_32.0K.json",
-    "sp50T": "tokenizers/sp/TSP_tokenizer_50.0K.model",
-    "sp64T": "tokenizers/sp/TSP_tokenizer_64.0K.model",
-    "sp86T": "tokenizers/sp/TSP_tokenizer_86.0K.model"
+    "sp50T": "tokenizers/sp/TSP_tokenizer_50.0K.json",
+    "sp64T": "tokenizers/sp/TSP_tokenizer_64.0K.json",
+    "sp86T": "tokenizers/sp/TSP_tokenizer_86.0K.json"
 }
 
 def get_bpe(name):
